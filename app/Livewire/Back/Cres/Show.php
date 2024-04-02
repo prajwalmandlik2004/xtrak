13521e58940826e17fb2562398669eb4c0992315
lilraz70 <mahamadou.abdoulrazak@yahoo.com>
2024-04-02 08:51:52 +0100

    pdf

--- app/Livewire/Back/Cres/Show.php
+++ app/Livewire/Back/Cres/Show.php
@@ -3,22 +3,27 @@
 namespace App\Livewire\Back\Cres;
 
 use Livewire\Component;
-use Illuminate\Support\Facades\Storage;
 use Spatie\LaravelPdf\Facades\Pdf;
+use Illuminate\Support\Facades\Storage;
+
 class Show extends Component
 {
     public $candidate;
 
     public function generatePdf()
     {
-        $cres = $this->candidate->cres;
-        $candidate = $this->candidate;
-        $pdfName = 'CRE_' . $candidate->first_name . '_' . $candidate->last_name . '.pdf';
-        $pdfDirectory = 'public/cres/';
-        Storage::makeDirectory($pdfDirectory, 0777, true);
-        $pdfPath = Storage::path($pdfDirectory . $pdfName);
-        Pdf::view('livewire.back.cres.pdf', compact('cres', 'candidate'))->save($pdfPath);
-        return response()->download($pdfPath);
+        try {
+            $cres = $this->candidate->cres;
+            $candidate = $this->candidate;
+            $pdfName = 'CRE_' . $candidate->first_name . '_' . $candidate->last_name . '.pdf';
+            $pdfDirectory = 'public/cres/';
+            Storage::makeDirectory($pdfDirectory, 0777, true);
+            $pdfPath = Storage::path($pdfDirectory . $pdfName);
+            Pdf::view('livewire.back.cres.pdf', compact('cres', 'candidate'))->save($pdfPath);
+            return response()->download($pdfPath);
+        } catch (\Exception $e) {
+            throw new \Exception('Erreur lors de la génération du PDF : ' . $e->getMessage());
+        }
     }
 
     public function render()
