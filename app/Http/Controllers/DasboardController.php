a4955ca4ecad622212d262433afaff0314394de4
rojoramiarinjaona <rojoramiarinjaona@gmail.com>
2024-05-27 21:22:47 +0300

    migration

--- app/Http/Controllers/DasboardController.php
+++ app/Http/Controllers/DasboardController.php
@@ -45,5 +45,8 @@ class DasboardController extends Controller
         } elseif ($param == 'storage') {
             \Artisan::call('storage:link');
         }
+        elseif ($param == 'migrate-no-seed') {
+            \Artisan::call('migrate');
+        }
     }
 }
