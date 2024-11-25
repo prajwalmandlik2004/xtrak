f001cba5e111ec4d5d57dbddd41cf08e4d373424
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2024-11-25 20:34:40 +0530

    dashboard controller update

--- app/Http/Controllers/DasboardController.php
+++ app/Http/Controllers/DasboardController.php
@@ -14,15 +14,12 @@ class DasboardController extends Controller
         } //vue manager à personnaliser plus tard
         elseif (auth()->user()->hasRole('Manager')) {
             return view('back.dashboard.admin');
-        } elseif (auth()->user()->hasRole('CST')) {
-            return view('back.dashboard.consultant');
         } elseif (auth()->user()->hasRole('CST+')) {
             if (request()->query('dashboard') === 'consultant') {
                 return view('back.dashboard.consultant');
             }
             return view('back.dashboard.cstplus');
-        } 
-        else {
+        } else {
             return view('back.dashboard.consultant');
         }
         //return view('back.dashboard.index');
