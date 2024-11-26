a13013c66b8143f5ef9486ff82d3c8a2cfffc5ef
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2024-11-26 13:04:11 +0530

    controller update

--- app/Http/Controllers/DasboardController.php
+++ app/Http/Controllers/DasboardController.php
@@ -15,8 +15,10 @@ class DasboardController extends Controller
         elseif (auth()->user()->hasRole('Manager')) {
             return view('back.dashboard.admin');
         } elseif (auth()->user()->hasRole('CST+')) {
-            if (request()->query('dashboard') === 'consultant') {
-                return view('back.dashboard.consultant');
+            if (auth()->user()->hasRole('CST')){
+                if (request()->query('dashboard') === 'consultant') {
+                    return view('back.dashboard.consultant');
+                }
             }
             return view('back.dashboard.cstplus');
         } else {
