c8c62733194cc77ea287e27e887e71e92b18f809
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2024-12-31 00:11:38 +0530

    sidebar update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -27,14 +27,13 @@
 
      <div id="scrollbar">
          <div class="container-fluid">
-
              <div id="two-column-menu">
              </div>
-             <ul class="navbar-nav" id="navbar-nav">
+              <ul class="navbar-nav" id="navbar-nav">
                  <li class="menu-title"><span data-key="t-menu">Menu</span></li>
-                 <li class="nav-item {{ request()->routeIs('dashboard') ? 'bg-dash-sidebar' : '' }}">
-                     <a class="nav-link menu-link  {{ request()->routeIs('dashboard') ? '' : '' }}   {{ request()->routeIs('dashboard') ? 'active' : '' }}"
-                         href="{{ route('dashboard') }}">
+                 <li class="nav-item {{ request()->routeIs('landings') ? 'bg-dash-sidebar' : '' }}">
+                     <a class="nav-link menu-link  {{ request()->routeIs('landings') ? '' : '' }}   {{ request()->routeIs('lanings') ? 'active' : '' }}"
+                         href="{{ route('landings') }}">
                          <i class="ri-dashboard-2-line"></i>
                          <span data-key="t-dashboards">
                              @if (auth()->user()->hasRole('Administrateur'))
@@ -63,9 +62,10 @@
                  <!-- <a href="{{ route('cdtvue') }}" class="nav-link {{ request()->routeIs('cdtvue*') ? 'active' : '' }}" data-key="t-chat">CDTvue</a> -->
 
                  <a class="nav-link"
-                     href="#">
+                     href="/landings">
                      <i class="ri-apps-2-line"></i> <span data-key="t-vault" style="color:orange">LANDING</span>
                  </a>
+
 
 
 
