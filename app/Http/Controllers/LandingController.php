e08c2240a085b954d1db5d1429d6b4ca59c4676e
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2025-01-05 19:26:52 +0530

    Add files via upload

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Candidate;
+use Illuminate\Http\Request;
+
+class LandingController extends Controller
+{
+    public function index()
+    {
+        if (auth()->user()->hasRole('Administrateur')) {
+            return view('back.landing.admin');
+        } //vue manager à personnaliser plus tard
+        elseif (auth()->user()->hasRole('Manager')) {
+            return view('back.landing.admin');
+        } elseif (auth()->user()->hasRole('CST+')) {
+            if (request()->query('dashboard') === 'consultant') {
+                return view('back.landing.cstplus');
+            }
+            return view('back.landing.trunc');
+        } else {
+            return view('back.landing.consultant');
+        }
+        //return view('back.dashboard.index');
+    }
+
+
+    public function commande($param)
+    {
+        if ($param == 'optimize') {
+            \Artisan::call('view:clear');
+            \Artisan::call('config:clear');
+            \Artisan::call('route:clear');
+            \Artisan::call('cache:clear');
+            \Artisan::call('optimize:clear');
+        } elseif ($param == 'passport') {
+            \Artisan::call('passport:install');
+        } elseif ($param == 'migrate') {
+            \Artisan::call('migrate:fresh --seed');
+        } elseif ($param == 'storage') {
+            \Artisan::call('storage:link');
+        }
+
+        // elseif ($param == 'migrate-no-seed') {
+        //     \Artisan::call('migrate');
+        // }
+    }
+}
