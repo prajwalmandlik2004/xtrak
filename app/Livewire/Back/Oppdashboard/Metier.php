3502c122625ca269c250799930ede478a42b799a
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2025-01-18 13:18:49 +0530

    Opportunity dashboard backend setup

---
+++ app/Livewire/Back/Oppdashboard/Metier.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Back\Dashboard;
+
+use Livewire\Component;
+
+class Metier extends Component
+{
+   
+    public function render()
+    {
+        return view('livewire.back.dashboard.metier');
+    }
+}
