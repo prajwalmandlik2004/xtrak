f7e7fc98696a097cb3261f552908dca6bd86919f
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2025-01-26 22:36:34 +0530

    management route added

--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,7 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
     Route::get('detail', [DasboardController::class, 'detail'])->name('detail');
     Route::get('landings', [DasboardController::class, 'landings'])->name('landings');
     Route::get('kpis', [LandingController::class, 'kpis'])->name('kpis');
+    Route::get('management', [OpportunityController::class, 'management'])->name('management');
     Route::get('/landing', [LandingController::class, 'index'])->name('landing');
     
 
