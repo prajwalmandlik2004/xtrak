7aab81e5eec36b3bd0c329d87f5750af1a624da4
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2025-02-24 20:13:52 +0530

    CTCdash model file added

---
+++ app/Models/Ctcdashboard.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ctcdashboard extends Model
+{
+    protected $table = 'ctc_vue';
+
+    protected $fillable = [
+        'date_ctc',
+        'company_ctc',
+        'civ',
+        'first_name',
+        'last_name',
+        'function_ctc',
+        'std_ctc',
+        'ext_ctc',
+        'ld',
+        'cell',
+        'mail'
+    ];
+}
