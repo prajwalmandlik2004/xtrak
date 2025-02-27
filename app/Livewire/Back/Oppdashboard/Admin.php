90f60d14289291bc7f3bcc810acbe9e5e7c718cd
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2025-02-27 19:17:44 +0530

    OPPvue records count backend

--- app/Livewire/Back/Oppdashboard/Admin.php
+++ app/Livewire/Back/Oppdashboard/Admin.php
@@ -13,6 +13,32 @@ class Admin extends Component
     public $sortField = 'updated_at';
     public $sortDirection = 'desc';
     public $selectAll = false;
+    public $hiredCount;
+    public $inprogressCount;
+    public $presentedCount;
+
+    public function mount()
+    {
+        $this->hiredCount = $this->countHired();
+        $this->inprogressCount = $this->countInprogress();
+        $this->presentedCount = $this->countPresented();
+    }
+
+
+    public function countHired()
+    {
+        return Oppdashboard::where('opportunity_status', 'HIRED')->count();
+    }
+
+    public function countInprogress()
+    {
+        return Oppdashboard::where('opportunity_status', 'En cours')->count();
+    }
+
+    public function countPresented()
+    {
+        return Oppdashboard::where('opportunity_status', 'Presented')->count();
+    }
 
     public function sortBy($field)
     {
