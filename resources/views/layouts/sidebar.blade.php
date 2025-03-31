0eaf44e111bb80afc37d621a11d4d8300b40f90e
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2025-03-31 22:43:49 +0530

    sidebar linking added

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -96,14 +96,17 @@
                              <li class="nav-item">
                                  <a href="{{ route('ctcdashboard') }}" class="nav-link {{ request()->routeIs('ctcdashboard*') ? 'active' : '' }}" data-key="t-chat"><span style="color:#09ff00">CTCvue</span></a>
                              </li>
+                             <li class="nav-item">
+                                 <a href="{{ route('cstdashboard') }}" class="nav-link {{ request()->routeIs('cstdashboard*') ? 'active' : '' }}" data-key="t-chat"><span style="color:#09ff00">CSTvue</span></a>
+                             </li>
 <!--                              <li class="nav-item">
                                  <a href="javascript:void(0);" onclick="alert('New features coming soon! ✅')" class="nav-link {{ request()->routeIs('facvue*') ? 'active' : '' }}" data-key="t-chat"><span style="color:orange">FACvue</span></a>
                              </li> -->
-                             <li class="nav-item">
+<!--                              <li class="nav-item">
                                  <a href="{{ route('dashboard', ['dashboard' => 'consultant']) }}" class="nav-link {{ request()->query('dashboard') === 'consultant' ? 'active' : '' }}" data-key="t-chat">
                                      <span style="color:#09ff00">CSTvue</span>
                                  </a>
-                             </li>
+                             </li> -->
                              @elseif (auth()->user()->hasRole('Manager'))
                              <li class="nav-item">
                                  <a href="{{ route('dashboard') }}" class="nav-link {{ request()->routeIs('dashboard') ? 'active' : '' }}" data-key="t-chat"><span style="color:#09ff00">CDTvue</span></a>
@@ -196,6 +199,9 @@
                                                      </li>
                                                      <li class="nav-item">
                                                          <a href="/ctcform" class="nav-link" data-key="t-chat"><span style="color:#09ff00">FormCTC</span></a>
+                                                     </li>
+                                                     <li class="nav-item">
+                                                         <a href="/cstform" class="nav-link" data-key="t-chat"><span style="color:#09ff00">FormCST</span></a>
                                                      </li>
                                                      @else
                                                      <li class="nav-item">
