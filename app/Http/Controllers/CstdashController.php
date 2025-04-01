20a0f9e386777b3c2d678358ae4b4fb8aeb593c5
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2025-04-01 12:47:19 +0530

    CSTdash controller added

---
+++ app/Http/Controllers/CstdashController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cstdashboard;
+
+class CstdashController extends Controller
+{
+    public function index()
+    {
+        $data = Cstdashboard::all();
+        return view('back.cstdashboard.admin', compact('data'));
+    }
+}
+
+
+
+
+
