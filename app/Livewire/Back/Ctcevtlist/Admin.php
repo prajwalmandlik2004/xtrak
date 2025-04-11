90b58f8e02359c7d5f81d64c3f9bc6a7cf0852d6
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2025-04-11 21:49:19 +0530

    backend for all the lists

---
+++ app/Livewire/Back/Ctcevtlist/Admin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Livewire\Back\Ctcevtlist;
+
+use App\Models\Trgdashboard;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Admin extends Component
+{
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+    public $sortField = 'updated_at';
+    public $sortDirection = 'desc';
+    public $selectAll = false;
+
+    public function sortBy($field)
+    {
+        if ($this->sortField === $field) {
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        } else {
+            $this->sortField = $field;
+            $this->sortDirection = 'asc';
+        }
+
+        $this->resetPage();
+    }
+
+    public function render()
+    {
+        $data = Trgdashboard::orderBy($this->sortField, $this->sortDirection)
+            ->paginate(100);
+
+        return view('livewire.back.ctcevtlist.admin', [
+            'data' => $data
+        ]);
+    }
+}
