2ae6ba7835918d4987c493efb1e3c5733aa3b87b
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2025-04-22 12:36:32 +0530

    update

--- app/Http/Controllers/OppCdtController.php
+++ app/Http/Controllers/OppCdtController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Trgdashboard;
+use App\Models\OppCdtLink;
 
 class OppCdtController extends Controller
 {
     public function index()
     {
-        $data = Trgdashboard::all();
+        $data = OppCdtLink::all();
         return view('back.oppcdtlist.admin', compact('data'));
     }
 }
