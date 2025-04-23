036221a19512560c8a9edee48a9efb17b000cce1
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2025-04-23 19:02:44 +0530

    small update

--- app/Models/Cstdashboard.php
+++ app/Models/Cstdashboard.php
@@ -19,6 +19,17 @@ class Cstdashboard extends Model
         'status',
         'notes',
     ];
+
+    public function oppLinks()
+    {
+        return $this->hasMany(OppCstLink::class, 'cst_id');
+    }
+
+    public function opportunities()
+    {
+        return $this->belongsToMany(Oppdashboard::class, 'opp_cst_links', 'cst_id', 'opp_id')
+            ->withTimestamps();
+    }
 }
 
 
