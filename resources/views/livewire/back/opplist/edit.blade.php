54b0a67f2adfa1fbdf8ec33f399fe3714af4e523
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2025-05-12 22:18:15 +0530

    button color update

--- resources/views/livewire/back/opplist/edit.blade.php
+++ resources/views/livewire/back/opplist/edit.blade.php
@@ -1367,7 +1367,7 @@
         }
 
         .btn-valid {
-            background-color: #6F61C0;
+            background-color: #00CCDD;
             color: white;
         }
 
@@ -1382,7 +1382,7 @@
         }
 
         .btn-valid:hover {
-            background-color: #6F61C0;
+            background-color: #00CCDD;
             color: white;
         }
 
